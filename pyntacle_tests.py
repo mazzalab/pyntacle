9a841ce1b440d8a3691f7f98c7e18129050581ec
wariobrega <wariobrega@gmail.com>
2018-03-09 13:20:57 +0100

    prova gitignore

--- pyntacle_tests.py
+++
@@ -1,6 +0,0 @@
-from pyntacle_commands_utils.kpsearch_wrapper_NEW import KPWrapper
-from misc.enums import KPNEGchoices
-from io_stream.importer import PyntacleImporter as pi
-from igraph import Graph
-
-graph = pi.AdjacencyMatrix(file="", sep="\t", header=True)
