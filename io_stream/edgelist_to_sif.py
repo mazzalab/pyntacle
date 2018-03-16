b82d980de1295a6446e5982d9229770b89df3cc4
wariobrega <wariobrega@gmail.com>
2018-03-16 16:13:39 +0100

    reporter almost reworked; edgelist to sif still unfixed, missing some local topology metrics

--- io_stream/edgelist_to_sif.py
+++ io_stream/edgelist_to_sif.py
@@ -9,77 +9,56 @@ __date__ = "27 February 2018"
 __license__ = u"""
   Copyright (C) 2016-2018  Tommaso Mazza <t.mazza@css-mendel.it>
   Viale Regina Margherita 261, 00198 Rome, Italy
-
   This program is free software; you can use and redistribute it under
   the terms of the BY-NC-ND license as published by
   Creative Commons; either version 4 of the License, or
   (at your option) any later version.
-
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   License for more details.
-
   You should have received a copy of the license along with this
   work. If not, see http://creativecommons.org/licenses/by-nc-nd/4.0/.
   """
+
+import os
+import sys
+
+import pandas as pd
+from config import *
+from utils import edgelist_utils
 
 '''
 this class covers the processing of an edgelist to a  simple interaction format (sif) that can be passed to cytoscape
 '''
 
-import pandas as pd
-from config import *
-from tools.edgelist_utils import EglUtils as egl
-from misc.import_utils import *
-from exceptions.unproperlyformattedfile_error import UnproperlyFormattedFileError
-# Checking presence of pycairo, needed for plotting
-from importlib import util
-pycairo_check = util.find_spec("cairo")
-if pycairo_check is None:
-    raise EnvironmentError("pyntacle needs the pycairo library to be installed and available "
-                           "in order to produce plots. Please install it and try again.")
 
-#todo rework all this
-class QuickConvert:
+# todo rework all this
+class EdgeListToCytoscape():
     '''
-    This class wis designed to quickly convert one file format to another, wuthout passing to the Graph object
-    Importing/Exporting. At the moment, we quickly convert
+    This class converts an edge list into a sif file (simple interaction format) in the most basic way:
+    a tab delimited file in which each line represent an edge (so node A \t node B)
     '''
-
-    @staticmethod
-    @filechecker
-    def EdgelistToSif(input_file:str, sep=None, header=False,):
-        eglutils = egl(file=input_file, header=header, separator=sep)
-
-        if eglutils.is_direct():
-            raise UnproperlyFormattedFileError("Edge List is direct")
-
-        if eglutils.is_pyntacle_ready():
-            raise UnproperlyFormattedFileError("Edgelist contains multiple edges contaioning two vertices")
-
-        checkfile = pd.read_csv(filepath_or_buffer=input_file, sep=sep)
-        if len(checkfile.columns) != 2:
-            raise UnproperlyFormattedFileError("Input file is not an edgelist (does not have 2 columns")
-
-        #todo arrivato qua, finisci questo
-        self.edgelist = []
-        with open(self.infile, "r") as inf:
-            if self.headerflag:
-                self.headerstring = inf.readline().rstrip().split(self.separator)
-                # print(self.headerstring)
-                # input()
-
-            for line in inf:
-                pair = line.rstrip().split(self.separator)
-                self.edgelist.append(pair)  # a list of lists
-
-    @staticmethod
-    def SifToEdgelist((input_file:str, header=False, separator=None):
-        pass
 
     def __init__(self, input_file: str, header=False, separator=None):
         self.logger = log
+
+        if not os.path.exists(input_file):
+            self.logger.error("The input file does not exist")
+            raise FileNotFoundError("File does not exist")
+
+        else:
+
+            eglutils = edgelist_utils.EglUtils(file=input_file, header=header, separator=separator)
+            isdirect = eglutils.is_direct()
+            ismulti = eglutils.is_pyntacle_ready()
+
+            if isdirect:
+                self.logger.warning("The edge list is not direct.")
+
+            if ismulti:
+                self.logger.warning(
+                    "The edge list is a multigraph. Be advised that it will not work with other pyntacle Features")
 
             self.infile = input_file
             self.headerflag = header
@@ -97,12 +76,31 @@ class QuickConvert:
                     self.separator = separator
 
             # check that the file contains 2 columns
+            checkfile = pd.read_csv(filepath_or_buffer=self.infile, sep=self.separator)
+            if len(checkfile.columns) != 2:
+                self.logger.error("Input file is not an edgelist (does not have 2 columns")
+                sys.exit(1)
 
+    def __edgelist_parser(self):
+        '''
+        Hidden function that stores and edgelist into a list of lists
+        '''
+
+        self.edgelist = []
+        with open(self.infile, "r") as inf:
+            if self.headerflag:
+                self.headerstring = inf.readline().rstrip().split(self.separator)
+                # print(self.headerstring)
+                # input()
+
+            for line in inf:
+                pair = line.rstrip().split(self.separator)
+                self.edgelist.append(pair)  # a list of lists
 
     def get_sif(self, output_file=None, separator=None, header=False):
         '''
         **[EXPAND]**
-        
+
         :param output_file: path to output file. Default: None. If not specified, a file with the same name will be used but with the .sif extension
         :param separator: which separator to use when building a sif file. Default is "\t"
         '''
@@ -141,5 +139,6 @@ class QuickConvert:
                 outfile.write(separator.join([couple[0], "interacts_with", couple[1]]) + "\n")
 
         self.logger.info("file converted")
-        self.logger.info("path to file is %s".format(output_file))
 
+
+self.logger.info("path to file is %s".format(output_file))
