088c5a3b8c8ea6ec5f942cf60ecac957ad21da17
wariobrega <wariobrega@gmail.com>
2018-03-23 19:32:39 +0100

    implementing parallel brute-force search (copied from Tom's version)

--- algorithms/bruteforce_search_NEW.py
+++ algorithms/bruteforce_search_NEW.py
@@ -189,3 +189,320 @@ class BruteforceSearch:
 
 #todo missing stuff:
 #todo Tom: implement bruteforce parallel you wrote pleaseeeee!
+
+
+def crunch_fragmentation_combinations(allS, graph: Graph, kpp_type: _KeyplayerAttribute) -> dict:
+    kppset_score_pairs = {}
+    """: type: dic{(), float}"""
+
+    # print("{}: {}".format(os.getpid(), len(allS)))
+
+    temp_graph = graph.copy()
+    temp_graph.delete_vertices(allS)
+    if kpp_type == _KeyplayerAttribute.F:
+        kpp_func = KeyPlayer.F
+    else:
+        kpp_func = KeyPlayer.DF
+
+    kppset_score_pairs[allS] = kpp_func(temp_graph)
+    return kppset_score_pairs
+
+
+def crunch_reachability_combinations(allS, graph: Graph, kpp_type: _KeyplayerAttribute, m=None) -> dict:
+    kppset_score_pairs = {}
+    """: type: dic{(), float}"""
+
+    # print("{}: {}".format(os.getpid(), len(allS)))
+
+    if kpp_type == _KeyplayerAttribute.MREACH:
+        reachability_score = KeyPlayer.mreach(m, index_list=allS, recalculate=True)
+    else:
+        reachability_score = KeyPlayer.DR(node_names_list=allS, recalculate=True)
+
+    kppset_score_pairs[allS] = reachability_score
+
+    return kppset_score_pairs
+
+
+import multiprocessing as mp
+
+# class BruteforceSearch:
+#     """
+#     **Brute-force search for the best kp-set**
+#     This class performs the search of the best kp set for each kp metric using a brute force algorithm
+#     specifically, it differs from the greedy_optimization class by finding ALL the best solutions for the
+#     kp-mmetrics questioned. It is divided in two methods: :func:`bruteforce_fragmentation`'s for finding best kp-neg
+#     set (and its relative values) and :func:'bruteforce_reachability's for finding the best kp-pos set (and its relative value)
+#     """
+#
+#     __graph = None
+#     """:type: Graph"""
+#
+#     def __init__(self, graph: Graph):
+#         """
+#         Initializes a graph for brute-force search of the best KP-set
+#         :param Graph graph: Graph provided in input
+#         :raises IllegalGraphSizeError: if the graph does not contain vertices or edges
+#         """
+#         self.logger = log
+#
+#         if graph.vcount() < 1:
+#             self.logger.fatal("This graph does not contain vertices")
+#             raise IllegalGraphSizeError("This graph does not contain vertices")
+#         elif graph.ecount() < 1:
+#             self.logger.fatal("This graph does not contain edges")
+#             raise IllegalGraphSizeError("This graph does not contain edges")
+#         else:
+#             self.__graph = graph
+#             GraphUtils.graph_checker(graph=self.__graph)
+#
+#     def bruteforce_fragmentation_parallel(self, kpp_size, kpp_type: _KeyplayerAttribute, ncore=mp.cpu_count()) -> (
+#     list, float):
+#         """
+#         It searches and finds the kpp-set of a predefined dimension that best disrupts the graph.
+#         It generates all the possible kpp-sets and calculates the fragmentation score of the residual graph, after
+#         having extracted the nodes belonging to the kpp-set.
+#         The best kpp-set will be the one that maximizes the fragmentation of the graph.
+#
+#         :param ncore: Number of CPU cores for parallel computing calculation of the maximum fragmentation score
+#         :param kpp_size: the size of the kpp-set
+#         :type kpp_size: int
+#         :param kpp_type: Either KeyplayerAttribute.DF or KeyplayerAttribute.F
+#         :type kpp_type: KeyplayerAttribute.name
+#         :return: - maxkpp_tuple_names: A list of group of nodes (names) of size **kpp_size** achieving the same maximum fragmentation score
+#                  - best_fragmentation_score: The achieved maximum fragmentation score
+#         :rtype: (list[list[str]], float)
+#         :raises TypeError: When the kpp-set size is not an integer number
+#         :raises WrongArgumentError: When the kpp-type argument is not of type KeyplayerAttribute.F or KeyplayerAttribute.DF
+#         """
+#         if not isinstance(kpp_size, int):
+#             self.logger.error("The kpp_size argument ('{}') is not an integer number".format(kpp_size))
+#             raise TypeError("The kpp_size argument ('{}') is not an integer number".format(kpp_size))
+#
+#         elif kpp_type != _KeyplayerAttribute.F and kpp_type != _KeyplayerAttribute.DF:
+#             self.logger.error(
+#                 "The kpp_type argument ('{}') must be of type KeyplayerAttribute.F or KeyplayerAttribute.DF".format(
+#                     kpp_type))
+#             raise TypeError(
+#                 "The kpp_type argument ('{}') must be of type KeyplayerAttribute.F or KeyplayerAttribute.DF".format(
+#                     kpp_type))
+#
+#         elif kpp_size >= self.__graph.vcount():
+#             self.logger.error("The kpp_size must be strictly less than the graph size")
+#             raise IllegalKppsetSizeError("The kpp_size must be strictly less than the graph size")
+#
+#         else:
+#             self.logger.info("Brute-force search of the best kpp-set of size {}".format(kpp_size))
+#
+#             if ncore == 1:
+#                 # let's call and return results from the serial implementation
+#                 return self.bruteforce_fragmentation(kpp_size, kpp_type)
+#             else:
+#                 kppset_score_pairs = {}
+#                 """: type: dic{(), float}"""
+#
+#                 # Generation of all combinations of nodes (all kpp-sets) of size kpp_size
+#                 node_indices = self.__graph.vs.indices
+#                 allS = itertools.combinations(node_indices, kpp_size)
+#
+#                 pool = mp.Pool(ncore)
+#                 for partial_result in pool.imap_unordered(partial(crunch_fragmentation_combinations,
+#                                                                   graph=self.__graph,
+#                                                                   kpp_type=kpp_type),
+#                                                           allS):
+#                     kppset_score_pairs = {**kppset_score_pairs, **partial_result}
+#
+#                 pool.close()
+#                 pool.join()
+#
+#                 best_fragmentation_score = max(kppset_score_pairs.values())
+#                 maxkpp_node_names = [self.__graph.vs(k)["name"] for k, v in kppset_score_pairs.items() if
+#                                      v == best_fragmentation_score]
+#                 self.logger.info("The best kpp-set of size {} {} {} with score {}".format(kpp_size,
+#                                                                                           'are' if len(
+#                                                                                               maxkpp_node_names) > 1 else 'is',
+#                                                                                           ', '.join([''.join(m) for m in
+#                                                                                                      maxkpp_node_names]),
+#                                                                                           best_fragmentation_score))
+#                 return maxkpp_node_names, best_fragmentation_score
+#
+#     def bruteforce_fragmentation(self, kpp_size, kpp_type) -> (list, float):
+#         """
+#         It searches and finds the kpp-set of a predefined dimension that best disrupts the graph.
+#         It generates all the possible kpp-sets and calculates the fragmentation score of the residual graph, after
+#         having extracted the nodes belonging to the kpp-set.
+#         The best kpp-set will be the one that maximizes the fragmentation of the graph.
+#
+#         :param kpp_size: the size of the kpp-set
+#         :type kpp_size: int
+#         :param kpp_type: Either KeyplayerAttribute.DF or KeyplayerAttribute.F
+#         :type kpp_type: KeyplayerAttribute.name
+#         :return: - maxkpp_node_names: A list of group of nodes (names) of size **kpp_size** achieving the same maximum fragmentation score
+#                  - best_fragmentation_score: The achieved maximum fragmentation score
+#         :rtype: (list[list[str]], float)
+#         :raises TypeError: When the kpp-set size is not an integer number
+#         :raises WrongArgumentError: When the kpp-type argument is not of type KeyplayerAttribute.F or KeyplayerAttribute.DF
+#         """
+#         if not isinstance(kpp_size, int):
+#             self.logger.error("The kpp_size argument ('{}') is not an integer number".format(kpp_size))
+#             raise TypeError("The kpp_size argument ('{}') is not an integer number".format(kpp_size))
+#         elif kpp_type != _KeyplayerAttribute.F and kpp_type != _KeyplayerAttribute.DF:
+#             self.logger.error(
+#                 "The kpp_type argument ('{}') must be of type KeyplayerAttribute.F or KeyplayerAttribute.DF".format(
+#                     kpp_type))
+#             raise TypeError(
+#                 "The kpp_type argument ('{}') must be of type KeyplayerAttribute.F or KeyplayerAttribute.DF".format(
+#                     kpp_type))
+#         elif kpp_size >= self.__graph.vcount():
+#             self.logger.error("The kpp_size must be strictly less than the graph size")
+#             raise IllegalKppsetSizeError("The kpp_size must be strictly less than the graph size")
+#         else:
+#             self.logger.info("Brute-force search of the best kpp-set of size {}".format(kpp_size))
+#
+#             kppset_score_pairs = {}
+#             """: type: dic{(), float}"""
+#
+#             # Generation of all combinations of nodes (all kpp-sets) of size kpp_size
+#             node_indices = self.__graph.vs.indices
+#             allS = itertools.combinations(node_indices, kpp_size)
+#
+#             for S in allS:
+#                 temp_graph = self.__graph.copy()
+#                 temp_graph.delete_vertices(S)
+#                 if kpp_type == _KeyplayerAttribute.F:
+#                     kpp_func = KeyPlayer.F
+#                 else:
+#                     kpp_func = KeyPlayer.DF
+#
+#                 kppset_score_pairs[tuple(S)] = kpp_func(temp_graph)
+#
+#             best_fragmentation_score = max(kppset_score_pairs.values())
+#             maxkpp_node_names = [self.__graph.vs(k)["name"] for k, v in kppset_score_pairs.items() if
+#                                  v == best_fragmentation_score]
+#             self.logger.info("The best kpp-set of size {} {} {} with score {}".format(kpp_size,
+#                                                                                       'are' if len(
+#                                                                                           maxkpp_node_names) > 1 else 'is',
+#                                                                                       ', '.join([''.join(m) for m in
+#                                                                                                  maxkpp_node_names]),
+#                                                                                       best_fragmentation_score))
+#             return maxkpp_node_names, best_fragmentation_score
+#
+#     def bruteforce_reachability_parallel(self, kpp_size, kpp_type, m=None, ncore=mp.cpu_count()) -> (list, float):
+#         """
+#         It searches and finds the kpp-set of a predefined dimension that best reaches all other nodes over the graph.
+#         It generates all the possible kpp-sets and calculates their reachability scores.
+#         The best kpp-set will be the one that best reaches all other nodes of the graph.
+#
+#         .. note:: **m-reach**: min = 0 (unreachable); max = size(graph) - kpp_size (total reachability)
+#
+#                        **DR**: min = 0 (unreachable); max = 1 (total reachability)
+#
+#         :param int kpp_size: size of the kpp-set
+#         :param int m: maximum path length between the kpp-set and the other nodes of the graph
+#         :param KeyplayerAttribute.name kpp_type: Either KeyplayerAttribute.mreach or KeyplayerAttribute.DR
+#         :param ncore: Number of CPU cores for parallel computing calculation of the maximum fragmentation score
+#         :raises TypeError: When the kpp-set size is greater than the graph size
+#         :raises WrongArgumentError: When the kpp-type argument is not of type KeyplayerAttribute.mreach or KeyplayerAttribute.DR
+#         """
+#
+#         if not isinstance(kpp_size, int):
+#             self.logger.error("The kpp_size argument ('{}') is not an integer number".format(kpp_size))
+#             raise TypeError("The kpp_size argument ('{}') is not an integer number".format(kpp_size))
+#         elif kpp_size >= self.__graph.vcount():
+#             self.logger.error("The kpp_size must be strictly less than the graph size")
+#             raise IllegalKppsetSizeError("The kpp_size must be strictly less than the graph size")
+#         elif kpp_type != _KeyplayerAttribute.DR and kpp_type != _KeyplayerAttribute.MREACH:
+#             self.logger.error(
+#                 "The kpp_type argument ('{}') must be of type KeyplayerAttribute.DR or KeyplayerAttribute.MREACH".format(
+#                     kpp_type))
+#             raise TypeError(
+#                 "The kpp_type argument ('{}') must be of type KeyplayerAttribute.DR or KeyplayerAttribute.MREACH".format(
+#                     kpp_type))
+#         else:
+#             self.logger.info("Brute-force search of the best kpp-set of size {}".format(kpp_size))
+#
+#             kppset_score_pairs = {}
+#             """: type: dic{(), float}"""
+#
+#             # Generation of all combinations of nodes (all kpp-sets) of size kpp_size
+#             allS = itertools.combinations(self.__graph.vs.indices, kpp_size)
+#
+#             pool = mp.Pool(ncore)
+#             for partial_result in pool.imap_unordered(partial(crunch_reachability_combinations,
+#                                                               graph=self.__graph,
+#                                                               kpp_type=kpp_type,
+#                                                               m=m),
+#                                                       allS):
+#                 kppset_score_pairs = {**kppset_score_pairs, **partial_result}
+#             pool.close()
+#             pool.join()
+#
+#             best_reachability_score = max(kppset_score_pairs.values())
+#             maxkpp_node_names = [self.__graph.vs(k)["name"] for k, v in kppset_score_pairs.items() if
+#                                  v == best_reachability_score]
+#             self.logger.info("The best kpp-set of size {} {} {} with score {}".format(kpp_size,
+#                                                                                       'are' if len(
+#                                                                                           maxkpp_node_names) > 1 else 'is',
+#                                                                                       ', '.join([''.join(m) for m in
+#                                                                                                  maxkpp_node_names]),
+#                                                                                       best_reachability_score))
+#             return maxkpp_node_names, best_reachability_score
+#
+#     def bruteforce_reachability(self, kpp_size, kpp_type, m=None) -> (list, float):
+#         """
+#         It searches and finds the kpp-set of a predefined dimension that best reaches all other nodes over the graph.
+#         It generates all the possible kpp-sets and calculates their reachability scores.
+#         The best kpp-set will be the one that best reaches all other nodes of the graph.
+#
+#         .. note:: **m-reach**: min = 0 (unreachable); max = size(graph) - kpp_size (total reachability)
+#
+#                        **DR**: min = 0 (unreachable); max = 1 (total reachability)
+#
+#         :param int kpp_size: size of the kpp-set
+#         :param int m: maximum path length between the kpp-set and the other nodes of the graph
+#         :param KeyplayerAttribute.name kpp_type: Either KeyplayerAttribute.mreach or KeyplayerAttribute.DR
+#         :raises TypeError: When the kpp-set size is greater than the graph size
+#         :raises WrongArgumentError: When the kpp-type argument is not of type KeyplayerAttribute.mreach or KeyplayerAttribute.DR
+#         """
+#
+#         if not isinstance(kpp_size, int):
+#             self.logger.error("The kpp_size argument ('{}') is not an integer number".format(kpp_size))
+#             raise TypeError("The kpp_size argument ('{}') is not an integer number".format(kpp_size))
+#         elif kpp_size >= self.__graph.vcount():
+#             self.logger.error("The kpp_size must be strictly less than the graph size")
+#             raise IllegalKppsetSizeError("The kpp_size must be strictly less than the graph size")
+#         elif kpp_type != _KeyplayerAttribute.DR and kpp_type != _KeyplayerAttribute.MREACH:
+#             self.logger.error(
+#                 "The kpp_type argument ('{}') must be of type KeyplayerAttribute.DR or KeyplayerAttribute.MREACH".format(
+#                     kpp_type))
+#             raise TypeError(
+#                 "The kpp_type argument ('{}') must be of type KeyplayerAttribute.DR or KeyplayerAttribute.MREACH".format(
+#                     kpp_type))
+#         else:
+#             self.logger.info("Brute-force search of the best kpp-set of size {}".format(kpp_size))
+#
+#             kppset_score_pairs = {}
+#             """: type: dic{(), float}"""
+#
+#             # Generation of all combinations of nodes (all kpp-sets) of size kpp_size
+#             node_indices = self.__graph.vs.indices
+#             allS = itertools.combinations(node_indices, kpp_size)
+#
+#             for S in allS:
+#                 if kpp_type == _KeyplayerAttribute.MREACH:
+#                     reachability_score = KeyPlayer.mreach(self.__graph, index_list=list(S), m=m)
+#                 else:
+#                     reachability_score = KeyPlayer.DR(self.__graph, node_names_list=list(S))
+#
+#                 kppset_score_pairs[tuple(S)] = reachability_score
+#
+#             best_reachability_score = max(kppset_score_pairs.values())
+#             maxkpp_node_names = [self.__graph.vs(k)["name"] for k, v in kppset_score_pairs.items() if
+#                                  v == best_reachability_score]
+#             self.logger.info("The best kpp-set of size {} {} {} with score {}".format(kpp_size,
+#                                                                                       'are' if len(
+#                                                                                           maxkpp_node_names) > 1 else 'is',
+#                                                                                       ', '.join([''.join(m) for m in
+#                                                                                                  maxkpp_node_names]),
+#                                                                                       best_reachability_score))
+#             return maxkpp_node_names, best_reachability_score
