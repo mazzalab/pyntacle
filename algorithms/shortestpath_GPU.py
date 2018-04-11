Fixed GPU implementation of shortest path calculation

algorithms/shortestpath_GPU.py
@@ -1,11 +1,11 @@
 __author__ = "Daniele Capocefalo, Mauro Truglio, Tommaso Mazza"
 __copyright__ = "Copyright 2018, The pyntacle Project"
 __credits__ = ["Ferenc Jordan"]
-__version__ = "0.0.1"
-__maintainer__ = "Daniele Capocefalo"
-__email__ = "d.capocefalo@css-mendel.it"
+__version__ = "0.0.2"
+__maintainer__ = "Tommaso Mazza"
+__email__ = "t.mazza@css-mendel.it"
 __status__ = "Development"
-__date__ = "27 February 2018"
+__date__ = "11/04/2018"
 __license__ = u"""
   Copyright (C) 2016-2018  Tommaso Mazza <t,mazza@css-mendel.it>
   Viale Regina Margherita 261, 00198 Rome, Italy
@@ -26,45 +26,44 @@ __license__ = u"""
 
 """ This is the shortest path calculation using GPU that should be imported if the GPU is available"""
 
-from config import *
-from numba import cuda,jit, uint16
+from numba import cuda
 import numpy as np
 
-class SPGpu:
-#todo rewrite to work only on upper or lower triangular matrix
-#todo: debug, result matrix doesn't look right on figure 8
-    @staticmethod
-    @cuda.jit#(argtypes='uint16[:, :], uint16[:, :]')
-    def shortest_path_GPU(adjmat, result):
-        """
-        Calculate the shortest paths of a graph for aa single nodes, a set of nodes or all nodes in the graph using
-        'Floyd-Warshall Implementation <https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm>'_. The formula
-        is implemented using the numba library and allows for parallelization using GPU on CUDA compatible graphics.
-        :param np.ndarray adjmat: a numpy.ndarray containing the graph stored in an adjacency. Disconnected nodes in the
-        matrix are represented as the total number of nodes in the graph + 1, while the diagonal must contain zeroes
-        (no self loops allowed).
-        :param nodes: a list containing the indices of the input adjacency matrix corresponding to the query nodes
-        :param result: a `np.ndarray` object that will store the result. The maximum size of the array is equal to the
-        *shape* of the input adjacency matrix
-        :return:
-        """
-        posx, posy = cuda.grid(2)
-        graph_size = result.shape[0]
-        if posx < graph_size and posy < graph_size:  # Check array boundaries
-            min_path = result[posx, posy]
 
-            posXY = min_path
+# class SPGpu:
+# #todo rewrite to work only on upper or lower triangular matrix
+# #todo: debug, result matrix doesn't look right on figure 8
+@cuda.jit('void(uint16[:, :], uint16[:, :])')
+def shortest_path_GPU(adjmat, result):
+    """
+    Calculate the shortest paths of a graph for aa single nodes, a set of nodes or all nodes in the graph using
+    'Floyd-Warshall Implementation <https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm>'_. The formula
+    is implemented using the numba library and allows for parallelization using GPU on CUDA compatible graphics.
+    :param np.ndarray adjmat: a numpy.ndarray containing the graph stored in an adjacency. Disconnected nodes in the
+    matrix are represented as the total number of nodes in the graph + 1, while the diagonal must contain zeroes
+    (no self loops allowed).
+    :param nodes: a list containing the indices of the input adjacency matrix corresponding to the query nodes
+    :param result: a `np.ndarray` object that will store the result. The maximum size of the array is equal to the
+    *shape* of the input adjacency matrix
+    :return:
+    """
+    posx, posy = cuda.grid(2)
+    graph_size = result.shape[0]
+    if posx < graph_size and posy < graph_size:  # Check array boundaries
+        min_path = result[posx, posy]
 
-            if posXY > 2:
-                # if posy in nodes:
-                for k in range(0, adjmat.shape[0]):
-                    posXK = adjmat[posx, k]
-                    posKY = adjmat[k, posy]
+        posXY = min_path
 
-                    if posXY > posXK + posKY:
-                        min_path = posXK + posKY
+        if posXY > 2:
+            # if posy in nodes:
+            for k in range(0, adjmat.shape[0]):
+                posXK = adjmat[posx, k]
+                posKY = adjmat[k, posy]
 
-                    if min_path == 2:
-                        break
+                if posXY > posXK + posKY:
+                    min_path = posXK + posKY
 
-                result[posx, posy] = min_path
+                if min_path == 2:
+                    break
+
+            result[posx, posy] = min_path

pyntacle_tests.py
@@ -42,12 +42,27 @@ if __name__ == '__main__':
         file=r'C:\Users\t.mazza\Desktop\CSS-Bioinformatics\pyntacle\test\test_sets\input\figure_8.txt', header=True)
     print("\nGraph in main")
     print(mat.summary())
-    from algorithms.bruteforce_search import BruteforceSearch
 
-    # bf = BruteforceSearch.fragmentation(mat, kpp_size=2, kpp_type=KPNEGchoices.dF, parallel=True)
-    bf2 = BruteforceSearch.reachability(mat, kpp_size=2, kpp_type=KPPOSchoices.dR, parallel=True)
+    start_adj = np.array(mat.get_adjacency().data, dtype=np.uint16)
+    result = np.zeros_like(start_adj, np.uint16)
+    result.fill(start_adj.shape[0]+1)
+    np.fill_diagonal(result, 1)
+    print(result)
 
-    a = 3
+    import math
+    from algorithms.shortestpath_GPU import shortest_path_GPU
+    threadsperblock = (16, 16)
+    blockspergrid_x = math.ceil(start_adj.shape[0] / threadsperblock[0])
+    blockspergrid_y = math.ceil(start_adj.shape[1] / threadsperblock[1])
+    blockspergrid = (blockspergrid_x, blockspergrid_y)
+
+    shortest_path_GPU[blockspergrid, threadsperblock](start_adj, result)
+    print(result)
+
+
+    # from algorithms.bruteforce_search import BruteforceSearch
+    # # bf = BruteforceSearch.fragmentation(mat, kpp_size=2, kpp_type=KPNEGchoices.dF, parallel=True)
+    # bf2 = BruteforceSearch.reachability(mat, kpp_size=2, kpp_type=KPPOSchoices.dR, parallel=True)
 
 # #
 # print('gpu')